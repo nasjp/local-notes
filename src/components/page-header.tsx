a6baa4ddc187886b6caa720869592825efc36a7c
nas <y.softvalley@gmail.com>
2025-09-22 11:20:27 +0900

    Local Notesの見出しの太さを調整

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from "./search-bar";
 export function PageHeader() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center mb-8">Local Notes</h1>
+      <h1 className="text-3xl font-medium text-center mb-8">Local Notes</h1>
       <SearchBar />
     </div>
   );
